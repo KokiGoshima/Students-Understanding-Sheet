3009ad72a80daa060783298e47c2e66be22f70ea
Koki Goshima <gosh.namnam0705@gmail.com>
2019-06-26 00:57:13 +0900

    migration追加

--- database/migrations/2019_06_25_153304_create_how_much_understands_table.php
+++ database/migrations/2019_06_25_153304_create_how_much_understands_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateHowMuchUnderstandsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('how_much_understands', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('email', 191)->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->integer('level');
+            $table->text('question')->nullable();
+            $table->integer('student_id');
+            $table->integer('unit_id');
             $table->timestamps();
         });
     }
@@ -31,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('how_much_understands');
     }
 }
